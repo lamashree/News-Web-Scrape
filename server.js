f40c8a8bc82e62838a7e979def52b39ee21a2885
admin <admin@admins-MacBook-Pro.local>
2020-01-25 14:26:06 -0800

    deployed

--- server.js
+++ server.js
@@ -88,7 +88,20 @@ app.get("/articles/", function(req, res) {
   db.Article.find({})
     .then(function(dbArticle) {
       // If we were able to successfully find Articles, send them back to the client
-      res.json(dbArticle);
+      res.json(dbArticle)
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+// Route for getting all Articles from the db
+app.get("/article/", function(req, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.render('Articles');
     })
     .catch(function(err) {
       // If an error occurred, send it to the client
