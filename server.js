a409830a88de1c2ee130e856fc2f788c8970a38e
admin <admin@admins-MacBook-Pro.local>
2020-02-25 22:25:31 -0800

    deploy

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = process.env.port || 3000;
+var port = process.env.PORT|| 3000;
 
 // Initialize Express
 var app = express();
@@ -184,4 +184,7 @@ app.post("/articles/:id", function(req, res) {
 });
 
 // Start the server
-app.listen(process.env.PORT || 3000) 
+var server = app.listen(process.env.PORT || 5000, function () {
+  var port = server.address().port;
+  console.log("Express is working on port " + port);
+});
